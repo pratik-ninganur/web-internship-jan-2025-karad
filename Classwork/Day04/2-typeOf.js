904b5450b06ded354e4b8677b713aa6ec65decd1
Pratik Ninganur <pratik.ninganur@sunbeaminfo.com>
2025-01-10 12:42:16 +0530

    Day05 Classwork Data Dom, Validations, Uploaded

---
+++ Classwork/Day04/2-typeOf.js
@@ -0,0 +1,25 @@
+       let i;
+        var j=11;
+        const k=13;
+        var y=111;
+        console.log(j);
+        console.log(typeof(j));
+
+        var j="abcd";
+        console.log(j);
+        console.log(typeof(j));
+
+        var j=true;
+        console.log(j);
+        console.log(typeof(j));
+
+        var j=null;
+        console.log(j);
+        console.log(typeof(j));
+
+        var j=undefined;
+        console.log(j);
+        console.log(typeof(j));
+
+        console.log(k);
+        console.log(i);
---
+++ Classwork/Day04/6_function.js
@@ -0,0 +1,20 @@
+function print()
+{
+    console.log("Hello Js!")
+}
+function add(x,y)
+{
+    return x+y;
+}
+function Sub(x,y)
+{
+    return x-y;
+}
+function mul(x,y)
+{
+    return x*y;
+}
+function div(x,y)
+{
+    return x/y;
+}
